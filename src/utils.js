6da4c71816b912001ff2f4e0fdcee285bd1997d5
Ewald Horn <ewaldhorn@gmail.com>
2025-02-19 12:04:31 +0200

    Cleaning up code

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 // ----------------------------------------------------------------------------
 export const isRandomTrue = () => {
-  return Math.random() * 100 > 50;
+  return Math.random() > 0.5;
 };
